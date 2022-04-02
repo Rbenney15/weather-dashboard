9104dd86cdfbe1bcc94098909d9166a780b7880a
Rob Benney <rob.benney@gmail.com>
2022-04-02 15:48:44 -0400

    fixes after working withTAs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,46 @@
+var cities = [];
 
-var inputEl = document.getElementById("city-input");
-var searchEl = document.getElementById("search-button");
-var clearEl = document.getElementById("clear-history");
-var nameEl = document.getElementById("city-name");
-var currentPicEl = document.getElementById("current-pic");
-var currentTempEl = document.getElementById("temperature");
-var currentHumidityEl = document.getElementById("humidity");
-var currentWindEl = document.getElementById("wind-speed");
-var currentUVEl = document.getElementById("UV-index");
-var historyEl = document.getElementById("history");
-var searchHistory = JSON.parse(localStorage.getItem("search")) || [];
-console.log(searchHistory);
+var cityFormEl=document.querySelector("#city-search-form");
+var cityInputEl=document.querySelector("#city");
+var weatherContainerEl=document.querySelector("#current-weather-container");
+var citySearchInputEl = document.querySelector("#searched-city");
+var forecastTitle = document.querySelector("#forecast");
+var forecastContainerEl = document.querySelector("#fiveday-container");
+var pastSearchButtonEl = document.querySelector("#past-search-buttons");
 
-//when search button is clicked, locate city name typed by user
-function getWeather(cityName) {
-    var queryUrl = "" + cityName + "" + APIKey;
-     
+var formSumbitHandler = function(event){
+    event.preventDefault();
+    var city = cityInputEl.value.trim();
+    if(city){
+        getCityWeather(city);
+        get5Day(city);
+        cities.unshift({city});
+        cityInputEl.value = "";
+    } else{
+        alert("Please enter a City");
+    }
+    saveSearch();
+    pastSearch(city);
 }
 
+var saveSearch = function(){
+    localStorage.setItem("cities", JSON.stringify(cities));
+};
 
-//execute get weather function to gather 5 day forcast
+var getCityWeather = function(city) {
+    var apiKey = 'f6698c6bc3e491ae5c183fe57e2a6522';
+    var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+    
+    fetch(apiUrl)
+    .then(function(response){
+        response.json().then(function(data){
+            displayWeather(data, city);
+        });
+    });
+};
 
-//parse response to display current conditions
+
+
 
 //using saved city name, get 5 day forecasr get request from weather api
 
